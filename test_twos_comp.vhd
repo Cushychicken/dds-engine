24d865698bc38597461f7ce9e9ed342ef806c6aa
Nash Reilly <nrc.reilly@gmail.com>
2021-08-13 14:40:42 -0400

    Completed testbench for twos complement

--- test_twos_comp.vhd
+++ test_twos_comp.vhd
@@ -1,89 +1,88 @@
-------------------------------------------------------------------------------------------------------------
--- File name   : memory_interface.vhd
+----------------------------------------------------------------------------
+-- File name   : twos_comp.vhd
 --
--- Project     : EELE367 - Logic Design
+-- Project     : cushychicken/dds-engine
 --               
--- Description : VHDL model of a 16 x 8-bit ROM memory system that has been initialized
+-- Description : Testbench - two's comp block
 --
 -- Author(s)   : Nash Reilly
 --
--- Date        : April 11, 2011
+-- Date        : August 12, 2021
 --
--- Note(s)     : Data load register
+-- Note(s)     : Tests twos_comp.vhd
 -- 
---              Address      Description
---              ----------------------------------
---              $0           
---               :           Read Only Memory 
---              $F            (16x8-bit)              
---
-------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
-entity test_rom is
-end entity test_rom;
+entity test_twos_comp is
+end entity test_twos_comp;
 
-architecture test_rom_arch of test_rom is
+architecture test_twos_comp_arch of test_twos_comp is
 
-  constant t_clk_per : time := 20 ns;  -- Period of a 50MHZ Clock
+	constant t_clk_per : time := 20 ns;  -- Period of a 50MHZ Clock
   
-  component rom_16x8_sync
-    port (clock   : in  std_logic;
-          enable  : in  std_logic;
-          address : in  std_logic_vector(3 downto 0);
-          data    : out std_logic_vector(7 downto 0));
-  end component;
+	component twos_comp
+		port (Clock   : in  STD_LOGIC;
+			  Reset	  : in	STD_LOGIC;
+			  Comp	  : in  STD_LOGIC;
+			  Data	  : in  STD_LOGIC_VECTOR(13 downto 0);
+			  Output  : out STD_LOGIC_VECTOR(13 downto 0));
+	end component;
 
-  signal  Clock_TB     : STD_LOGIC;
-  signal  Enable_TB    : STD_LOGIC;
-  signal  Address_TB   : STD_LOGIC_VECTOR(3 downto 0);
-  signal  Data_TB      : STD_LOGIC_VECTOR(7 downto 0);
+	signal  Clock_TB	: STD_LOGIC;
+	signal  Reset_TB	: STD_LOGIC;
+	signal  Comp_TB		: STD_LOGIC;
+	signal  Data_TB		: STD_LOGIC_VECTOR(13 downto 0);
+	signal  Output_TB	: STD_LOGIC_VECTOR(13 downto 0);
   
-  begin
+	signal  i	: integer;
+
+begin
   
   --Component Declaration
     
-    DUT1: rom_16x8_sync port map (clock   => Clock_TB, 
-                                  enable  => Enable_TB,
-                                  address => Address_TB,
-                                  data    => Data_TB);
+    DUT1: twos_comp port map (Clock		=> Clock_TB, 
+                              Reset		=> Reset_TB,
+							  Comp		=> Comp_TB,
+							  Data		=> Data_TB,
+                              Output	=> Output_TB);
    
     CLOCK_STIM : process
-       begin
-          Clock_TB <= '0'; wait for 0.5*t_clk_per; 
-          Clock_TB <= '1'; wait for 0.5*t_clk_per; 
-       end process CLOCK_STIM;
+    begin
+		Clock_TB <= '0'; wait for 0.5*t_clk_per; 
+		Clock_TB <= '1'; wait for 0.5*t_clk_per; 
+
+		-- Assertion ends simulation after 10ms
+		assert now < 10 ms report "Simulation Finished." severity FAILURE;
+    end process CLOCK_STIM;
        
-    ENABLE_STIM : process
-      begin
-        Enable_TB <= '1'; wait for 2*t_clk_per;
-        Enable_TB <= '0'; wait for 1*t_clk_per;
-      end process ENABLE_STIM;
+    RESET_STIM : process
+    begin
+        Reset_TB <= '0'; wait for 2*t_clk_per;
+        Reset_TB <= '1'; 
+		wait;
+    end process RESET_STIM;
     
-    ADDRESS_STIM : process
-      begin
-        Address_TB <= "0000"; wait for 3*t_clk_per;
-        Address_TB <= "0001"; wait for 3*t_clk_per;
-        Address_TB <= "0010"; wait for 3*t_clk_per;
-        Address_TB <= "0011"; wait for 3*t_clk_per;
-        Address_TB <= "0100"; wait for 3*t_clk_per;
-        Address_TB <= "0101"; wait for 3*t_clk_per;
-        Address_TB <= "0110"; wait for 3*t_clk_per;
-        Address_TB <= "0111"; wait for 3*t_clk_per;
-        Address_TB <= "1000"; wait for 3*t_clk_per;
-        Address_TB <= "1001"; wait for 3*t_clk_per;
-        Address_TB <= "1010"; wait for 3*t_clk_per;
-        Address_TB <= "1011"; wait for 3*t_clk_per;
-        Address_TB <= "1100"; wait for 3*t_clk_per;
-        Address_TB <= "1101"; wait for 3*t_clk_per;
-        Address_TB <= "1110"; wait for 3*t_clk_per;
-        Address_TB <= "1111"; wait for 3*t_clk_per;
-      end process;
+    DATA_STIM : process
+    begin
+		Data_TB <= "00000000000000";
+		Comp_TB <= '0';
+		wait for 3*t_clk_per;
+		while ( Data_TB < "11111111111111" ) loop
+			Comp_TB <= '1';
+			wait for t_clk_per;
+
+			Data_TB <= (Data_TB + 1); 
+			Comp_TB <= '0';
+			wait for t_clk_per;
+		end loop;
+		wait;
+    end process DATA_STIM;
       
-    end architecture;
-        
-        
+end architecture;
+
+
