b99f4af958c25509707d6955cb616bdf0c0f5d63
Nash Reilly <nrc.reilly@gmail.com>
2021-08-19 11:13:39 -0400

    Added 2-to-1 mux for freq/phase select

---
+++ src/mux_2in_1out.vhd
@@ -0,0 +1,65 @@
+---------------------------------------------------------------------------
+-- File name   : mux_2in_1out.vhd
+--
+-- Project     : cushychicken/dds-engine
+--
+-- Description : 2in, 1out mux to select freq/phase tuning words
+--
+-- Author(s)   : Nash Reilly (cushychicken@gmail.com)
+--
+-- Date        : Aug 18, 2021
+--
+-- Note(s)     :
+--
+----------------------------------------------------------------------------
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.std_logic_unsigned.all;
+  use ieee.std_logic_arith.all;
+
+entity mux_2in_1out is
+  port (
+    -- Clock/Reset
+    i_clock : in    std_logic;
+    i_reset : in    std_logic;
+
+    -- Mux Select Line
+    i_sel : in    std_logic;
+    -- Mux Input Ports
+    i_in0 : in    std_logic_vector(31 downto 0);
+    i_in1 : in    std_logic_vector(31 downto 0);
+    -- Mux Output Port
+    o_out : out   std_logic_vector(31 downto 0)
+  );
+end entity mux_2in_1out;
+
+architecture arch_mux of mux_2in_1out is
+
+  -- Signals and constants here
+
+begin
+
+  mux : process (i_clock, i_reset) is
+  begin
+
+    if (i_reset = '1') then
+      o_out <= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+    elsif (i_clock'event and i_clock='1') then
+
+      case (i_sel) is
+
+        when '0' =>
+          o_out <= i_in0;
+        when '1' =>
+          o_out <= i_in1;
+        when others =>
+		  o_out <= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+
+      end case;
+
+    end if;
+
+  end process mux;
+
+end architecture arch_mux;
---
+++ test/test_mux_2in_1out.vhd
@@ -0,0 +1,106 @@
+----------------------------------------------------------------------------
+-- File name   : test_mux_2in_1out.vhd
+--
+-- Project     : cushychicken/dds-engine
+--
+-- Description : Testbench - 32 bit multiplexer
+--
+-- Author(s)   : Nash Reilly
+--
+-- Date        : August 18, 2021
+--
+-- Note(s)     : Tests mux_2in_1out.vhd
+--
+----------------------------------------------------------------------------
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.std_logic_unsigned.all;
+  use ieee.std_logic_arith.all;
+
+entity test_mux_2in_1out is
+end entity test_mux_2in_1out;
+
+architecture test_mux_2in_1out_arch of test_mux_2in_1out is
+
+  constant t_clk_per : time := 20 ns; -- Period of a 50MHZ Clock
+
+  component mux_2in_1out is
+    port (
+      -- Clock/Reset
+      i_clock : in    std_logic;
+      i_reset : in    std_logic;
+
+      -- Mux Select Line
+      i_sel : in    std_logic;
+      -- Mux Input Ports
+      i_in0 : in    std_logic_vector(31 downto 0);
+      i_in1 : in    std_logic_vector(31 downto 0);
+      -- Mux Output Port
+      o_out : out   std_logic_vector(31 downto 0)
+    );
+  end component;
+
+
+  -- Testbench Signals
+  signal tb_clock  : std_logic;
+  signal tb_reset  : std_logic;
+  signal tb_sel   : std_logic;
+  signal tb_in0   : std_logic_vector(31 downto 0);
+  signal tb_in1   : std_logic_vector(31 downto 0);
+  signal tb_output : std_logic_vector(31 downto 0);
+
+begin
+
+  --Component Declaration
+  dut1 : component mux_2in_1out
+    port map (
+      i_clock  => tb_clock,
+      i_reset  => tb_reset,
+      i_sel   => tb_sel,
+      i_in0   => tb_in0,
+      i_in1   => tb_in1,
+      o_out => tb_output
+    );
+
+  clock_stim : process is
+  begin
+
+    tb_clock <= '0';
+    wait for 0.5 * t_clk_per;
+    tb_clock <= '1';
+    wait for 0.5 * t_clk_per;
+
+    -- Assertion ends simulation after 10ms
+    assert now < 1 us
+      report "Simulation Finished."
+      severity FAILURE;
+
+  end process clock_stim;
+
+  reset_stim : process is
+  begin
+
+    tb_reset <= '1';
+    wait for 2 * t_clk_per;
+    tb_reset <= '0';
+    wait;
+
+  end process reset_stim;
+
+  mux_stim : process is
+  begin
+
+    tb_in0   <= X"AAAA_AAAA";
+    tb_in1   <= X"5555_5555";
+	tb_sel	 <= '0';
+    wait for 3 * t_clk_per;
+
+	tb_sel   <= '1';
+    wait for 22* t_clk_per;
+    wait;
+  end process mux_stim;
+
+end architecture test_mux_2in_1out_arch;
+
+
